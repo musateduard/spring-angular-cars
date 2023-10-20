4998036f4a84b59fc6f248ee93eab80521d4d814
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-20 16:49:09 +0200

    added fields, getters and setters

--- backend/src/main/java/com/cars/backend/models/Car.java
+++ backend/src/main/java/com/cars/backend/models/Car.java
@@ -1,4 +1,64 @@
 package com.cars.backend.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
-public class Car {}
+
+@Entity
+@Table(name="car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="color")
+    private String color;
+
+    @Column(name="year")
+    private Integer year;
+
+    Car() {}
+
+    Car(String name, String color, Integer year) {
+        this.name = name;
+        this.color = color;
+        this.year = year;
+        return;}
+
+    public long getId() {
+        return this.id;}
+
+    public String getName() {
+        return this.name;}
+
+    public void setName(String name) {
+        this.name = name;}
+
+    public String getColor() {
+        return this.color;}
+
+    public void setColor(String color) {
+        this.color = color;}
+
+    public Integer getYear() {
+        return this.year;}
+
+    public void setYear(Integer year) {
+        this.year = year;}
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", color='" + getColor() + "'" +
+            ", year='" + getYear() + "'" +
+            "}";}}
