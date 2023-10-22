b76f8d0f808242a9bd1ba0be4808bcf089674c3c
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-22 18:38:51 +0200

    updated formatting

--- backend/src/main/java/com/cars/backend/models/Car.java
+++ backend/src/main/java/com/cars/backend/models/Car.java
@@ -1,64 +1,73 @@
 package com.cars.backend.models;
 
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
 
 
-@Entity
-@Table(name="car")
+@Entity(name = "car")
 public class Car {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "car_generator")
+    @SequenceGenerator(name = "car_generator", sequenceName = "car_id_seq", allocationSize = 1)
+    private Long id;
 
-    @Column(name="name")
+    @Column(length = 255, nullable = false)
     private String name;
 
-    @Column(name="color")
+    @Column(length = 255, nullable = false)
     private String color;
 
-    @Column(name="year")
+    @Column(nullable = false)
     private Integer year;
 
-    Car() {}
-
-    Car(String name, String color, Integer year) {
+    public Car(String name, String color, Integer year) {
         this.name = name;
         this.color = color;
         this.year = year;
-        return;}
+    }
 
-    public long getId() {
-        return this.id;}
+    public Car() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
-        return this.name;}
+        return name;
+    }
 
     public void setName(String name) {
-        this.name = name;}
+        this.name = name;
+    }
 
     public String getColor() {
-        return this.color;}
+        return color;
+    }
 
     public void setColor(String color) {
-        this.color = color;}
+        this.color = color;
+    }
 
     public Integer getYear() {
-        return this.year;}
+        return year;
+    }
 
     public void setYear(Integer year) {
-        this.year = year;}
+        this.year = year;
+    }
 
     @Override
     public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", name='" + getName() + "'" +
-            ", color='" + getColor() + "'" +
-            ", year='" + getYear() + "'" +
-            "}";}}
+        return "Car [id=" + id + ", name=" + name + ", color=" + color + ", year=" + year + "]";
+    }
+}
