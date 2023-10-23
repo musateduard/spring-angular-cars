f088f5280fe084a2b7bf51a6067425ef9f94815a
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-24 01:28:38 +0200

    added methods getData, setRender, ngOnInit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from './types';
 
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']})
-export class AppComponent {
-    title = 'frontend';}
+    styleUrls: ['./app.component.css'],
+    templateUrl: './app.component.html',})
+export class AppComponent implements OnInit {
+
+    title: string = 'frontend';
+    testString: string = "another app property";
+    testAnotherString: string = "one more props";
+    render: boolean = true;
+
+    testList: string[] = ["string 1", "string 2", "string 3", "string 4"];
+
+    renderData: boolean = false;
+    testData: Car[] = [];
+
+    ngOnInit(): void {
+        return;}
+
+    setRender(): void {
+        this.render = !this.render;
+        return;}
+
+    async getData(): Promise<void> {
+
+        console.log("fetching data");
+
+        try {
+            const response: Response = await fetch(
+
+                // request url
+                "http://localhost:9000/api/cars", {
+
+                // request headers
+                method: "GET",
+                mode: "cors",
+                headers: {"Content-Type": "application/json"},});
+
+            console.log(response);
+
+            const data: Car[] = await response.json();
+
+            console.log(data);
+
+            this.testData = data;
+            this.renderData = true;}
+
+        catch (error) {
+            console.log(error);}
+
+        // let time: Date = new Date();
+        // this.testData = ["data 1", "data 2", `${time.getUTCHours()}:${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`];
+        // this.renderData = true;
+
+        return;}}
