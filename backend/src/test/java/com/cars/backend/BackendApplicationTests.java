486d30f407d98b25908759db14202f8d77d2bcd9
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-26 18:24:47 +0200

    added selenium

--- backend/src/test/java/com/cars/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/cars/backend/BackendApplicationTests.java
@@ -2,6 +2,9 @@ package com.cars.backend;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,9 +12,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class BackendApplicationTests {
 
+	private static ChromeDriver browser;
+
+
+	@BeforeAll
+	public static void startBrowser() {
+
+		System.out.println("starting browser");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/com/cars/backend/chromedriver");
+		browser = new ChromeDriver();
+
+		return;}
+
+
+	@AfterAll
+	public static void quitBrowser() {
+
+		System.out.println("closing browser");
+		browser.quit();
+
+		return;}
+
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+
 		assertEquals(1, 1);
-		return;
-	}
+
+		browser.get("https://google.com");
+
+		try {
+			Thread.sleep(1000 * 5);}
+
+		catch (InterruptedException error) {
+			System.err.println(error);}
+
+		return;}
 }
