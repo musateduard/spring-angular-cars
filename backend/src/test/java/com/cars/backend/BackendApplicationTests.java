74402981d4474a5575844d21e0008c2bebacca10
Eduard Musat <144883995+musateduard@users.noreply.github.com>
2023-10-26 16:25:06 +0200

    updated contextLoads

--- backend/src/test/java/com/cars/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/cars/backend/BackendApplicationTests.java
@@ -1,13 +1,17 @@
 package com.cars.backend;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
+		return;
 	}
-
 }
