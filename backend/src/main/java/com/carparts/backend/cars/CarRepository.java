5c6c97e136aa8302731bba5ab50483e70e9a2d27
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-04 22:42:34 +0200

    removed findByName and findByYear from CarRepository.java

--- backend/src/main/java/com/carparts/backend/cars/CarRepository.java
+++ backend/src/main/java/com/carparts/backend/cars/CarRepository.java
@@ -1,13 +1,8 @@
 package com.carparts.backend.cars;
 
-import java.util.List;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 @Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
-
-    List<Car> findByName(String name);
-    List<Car> findByYear(Integer year);
-}
+public interface CarRepository extends JpaRepository<Car, Long> {}
