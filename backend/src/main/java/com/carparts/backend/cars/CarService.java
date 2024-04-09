c66a5e1edf79a4263528efa8e25c4a27e02ed8dc
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-09 22:41:13 +0200

    added updateCar to CarService.java

--- backend/src/main/java/com/carparts/backend/cars/CarService.java
+++ backend/src/main/java/com/carparts/backend/cars/CarService.java
@@ -1,8 +1,11 @@
 package com.carparts.backend.cars;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @Service
@@ -10,22 +13,6 @@ public class CarService {
 
     @Autowired
     private CarRepository carRepository;
-
-
-    public List<Car> getAllCars() {
-
-        System.out.println("getting all cars");
-        List<Car> carList = carRepository.findAll();
-
-        return carList;}
-
-
-//    public List<Car> getLada(String name) {
-//
-//        System.out.println("getting lada");
-//        List<Car> ladas = carRepository.findByName(name);
-//
-//        return ladas;}
 
 
     public Car createCar(Car carObject) {
@@ -37,16 +24,50 @@ public class CarService {
             System.out.println("creating dacia");}
 
         else {
-            System.out.println("creating other car");}
+            System.out.println("creating car");}
 
         Car response = carRepository.save(carObject);
 
         return response;}
 
 
-    public void deleteCar(Long carId) {
-        // todo: throw exception if car id not found
-        System.out.println("deleting car id: %d".formatted(carId));
+    public List<Car> getAllCars() {
+
+        System.out.println("getting all cars");
+        List<Car> carList = carRepository.findAll();
+
+        return carList;}
+
+
+    public Car getCarById(Long carId) throws ResponseStatusException {
+        System.out.println("getting car: %d".formatted(carId));
+        Car car = carRepository.findById(carId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return car;}
+
+
+    public Car updateCar(Long carId, Car requestBody) throws ResponseStatusException {
+
+        System.out.println("updating car: %d".formatted(carId));
+
+        // check if car exists
+        Car carToUpdate = carRepository.findById(carId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        // update fields of old car with request body
+        if (requestBody.getName() != null) {carToUpdate.setName(requestBody.getName());}
+        if (requestBody.getYear() != null) {carToUpdate.setYear(requestBody.getYear());}
+        if (requestBody.getColor() != null) {carToUpdate.setColor(requestBody.getColor());}
+
+        // save car to database
+        Car response = carRepository.save(carToUpdate);
+
+        return response;}
+
+
+    public void deleteCar(Long carId) throws ResponseStatusException {
+
+        System.out.println("deleting car: %d".formatted(carId));
+        carRepository.findById(carId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         carRepository.deleteById(carId);
+
         return;}
 }
