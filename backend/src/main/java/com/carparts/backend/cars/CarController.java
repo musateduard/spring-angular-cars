3557b93d9232b6b1ab91e77c852e79c3044125c7
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-09 22:40:49 +0200

    added updateCar to CarController.java

--- backend/src/main/java/com/carparts/backend/cars/CarController.java
+++ backend/src/main/java/com/carparts/backend/cars/CarController.java
@@ -4,29 +4,19 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 /** this is the car controller that includes methods for processing requests
  * for the cars component. */
 @RestController
-@CrossOrigin(origins="*")
-@RequestMapping(path="/api/cars")
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/api/cars")
 public class CarController {
 
     @Autowired
     private CarService carService;
-
-
-    @GetMapping
-    public ResponseEntity<List<Car>> getAllCars() {
-        List<Car> carList = carService.getAllCars();
-        return ResponseEntity.ok(carList);}
-
-
-//    @GetMapping(value="/ladas")
-//    public List<Car> getLada(String name) {
-//        List<Car> ladaList = carService.getLada("lada");
-//        return ladaList;}
 
 
     @PostMapping
@@ -35,9 +25,29 @@ public class CarController {
         return ResponseEntity.ok(createdCar);}
 
 
-    @DeleteMapping(value="/{carId}")
+    @GetMapping
+    public ResponseEntity<List<Car>> getAllCars() {
+        List<Car> carList = carService.getAllCars();
+        return ResponseEntity.ok(carList);}
+
+
+    @GetMapping(value = "/{carId}")
+    public ResponseEntity<Car> getById(@PathVariable Long carId) {
+        Car carResponse = carService.getCarById(carId);
+        return ResponseEntity.ok(carResponse);}
+
+
+    @PutMapping(value = "/{carId}")
+    public ResponseEntity<Car> updateCar(
+        @PathVariable Long carId,
+        @RequestBody Car requestBody) {
+
+        Car response = carService.updateCar(carId, requestBody);
+        return ResponseEntity.ok(response);}
+
+
+    @DeleteMapping(value = "/{carId}")
     public ResponseEntity<Void> deleteCar(@PathVariable Long carId) {
-        // todo: add error handling if car id not found
         carService.deleteCar(carId);
         return ResponseEntity.noContent().build();}
 }
