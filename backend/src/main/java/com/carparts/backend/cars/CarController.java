727a2960f3826daf415516048cd4176e0e765a58
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-15 01:18:25 +0200

    updated imports for CarController.java

--- backend/src/main/java/com/carparts/backend/cars/CarController.java
+++ backend/src/main/java/com/carparts/backend/cars/CarController.java
@@ -4,8 +4,6 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 /** this is the car controller that includes methods for processing requests
