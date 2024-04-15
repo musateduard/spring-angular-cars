2d64c0b109198c3ec17710753e274abc29f16dd8
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-15 20:48:58 +0200

    removed init_db.sh

--- database/init_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dropdb --if-exists car_parts &&
-createdb --template template0 --encoding "UTF8" --locale "en_US.UTF-8" --owner user1 car_parts &&
-psql -a car_parts user1 < schema.sql
