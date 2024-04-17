ecfcf84bb5d2bbe79c28d6a15e34a35d24b528d4
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-17 21:11:08 +0200

    added comments to schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,9 @@
-
-
 -- create main schema
 CREATE SCHEMA main;
 
 
 -- create owners table
+/* sdfjkhkjhsdfkhweuifghweuifhweuifh */
 CREATE TABLE IF NOT EXISTS main.owners (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -26,3 +25,14 @@ CREATE TABLE IF NOT EXISTS main.cars (
 CREATE TABLE IF NOT EXISTS main.previous_owners (
     car BIGINT NOT NULL REFERENCES main.cars(id) ON DELETE CASCADE,  -- should we cascade?
     owner BIGINT NOT NULL REFERENCES main.owners(id) ON DELETE CASCADE);  -- should we cascade?
+    -- start_date
+    -- end_date
+
+    -- date constraints
+    -- start_date < end_date by at least 1 day, always
+    -- gaps in ownership? allowed
+    -- overlap in ownership? not allowed
+    -- end_date in future? not allowed
+    -- end_date null? allowed
+    -- start_date in future? not allowed
+    -- start_date, end_date in past? allowed
