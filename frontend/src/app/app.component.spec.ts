1efc6fef47f21a474ab0171349949d0833b73ec7
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-19 01:16:44 +0200

    updated formatting

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,27 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
     beforeEach(() => TestBed.configureTestingModule({
         declarations: [AppComponent]
     }));
 
-    it('should create the app', () => {
+    it("should create the app", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'frontend'`, () => {
+    it("should have as title 'frontend'", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('frontend');
+        expect(app.title).toEqual("frontend");
     });
 
-    it('should render title', () => {
+    it("should render title", () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+        expect(compiled.querySelector(".content span")?.textContent).toContain("frontend app is running!");
     });
 });
