e7a610e1c0e1abfbd88bff4ac687acb4bfe22989
Musat Eduard <202c76ad05534d2aadc0defd28f751f5@email.com>
2024-04-19 01:13:11 +0200

    updated Car.java

--- backend/src/main/java/com/carparts/backend/cars/Car.java
+++ backend/src/main/java/com/carparts/backend/cars/Car.java
@@ -12,8 +12,6 @@ import jakarta.persistence.GenerationType;
 @Table(name = "cars", schema = "main")
 public class Car {
 
-    // todo: update car fields to new schema
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,55 +31,47 @@ public class Car {
     @Column(nullable = false)
     private Integer mileage;
 
-    // todo: mark field as reference to Owners table
-    private Long current_owner;
+//     todo: mark field as reference to Owners table
+//     private Long current_owner;
 
-    public Car(String name, String color, Integer year) {
+    public Car(String name, String color, Integer year, Integer horsepower, Integer mileage) {
 
         this.name = name;
         this.color = color;
         this.year = year;
+        this.horsepower = horsepower;
+        this.mileage = mileage;
 
         return;}
 
     public Car() {}
 
-    // todo: update getters and setters
+    public Long getId() {return id;}
 
-    public Long getId() {
-        return id;
-    }
+    public void setId(Long id) {this.id = id;}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getName() {return name;}
 
-    public String getName() {
-        return name;
-    }
+    public void setName(String name) {this.name = name;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getColor() {return color;}
 
-    public String getColor() {
-        return color;
-    }
+    public void setColor(String color) {this.color = color;}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+    public Integer getYear() {return year;}
 
-    public Integer getYear() {
-        return year;
-    }
+    public void setYear(Integer year) {this.year = year;}
 
-    public void setYear(Integer year) {
-        this.year = year;
-    }
+    public Integer getHorsepower() {return horsepower;}
+
+    public void setHorsepower(Integer horsepower) {this.horsepower = horsepower;}
+
+    public Integer getMileage() {return mileage;}
+
+    public void setMileage(Integer mileage) {this.mileage = mileage;}
 
     @Override
     public String toString() {
-        String carString = "Car [id=%d, name=%s, color=%s, year=%d]".formatted(id, name, color, year);
+        String carString = "Car [id=%d, name=%s, color=%s, year=%d, horsepower=%d, mileage=%d]".formatted(id, name, color, year, horsepower, mileage);
         return carString;}
 }
